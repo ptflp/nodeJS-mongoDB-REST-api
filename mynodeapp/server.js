74cc6a61a9a897eaef174c963aa51cc30764d1c6
ptflp <filippov.pa@s-vfu.ru>
2017-11-26 23:55:21 +0900

    set dependencies in server js

---
+++ mynodeapp/server.js
@@ -0,0 +1,5 @@
+// server.js
+const express        = require('express');
+const MongoClient    = require('mongodb').MongoClient;
+const bodyParser     = require('body-parser');
+const app            = express();
