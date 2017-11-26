cdc9c584ebfeeb9362eb9c4c2ca3dc598b4ce4ac
ptflp <filippov.pa@s-vfu.ru>
2017-11-27 01:36:10 +0900

    get notes by id

--- mynodeapp/app/routes/note_routes.js
+++ mynodeapp/app/routes/note_routes.js
@@ -1,4 +1,14 @@
 module.exports = function(app, db) {
+  app.get('/notes/:id', (req, res) => {
+    const details = { '_id': <ТУТ БУДЕТ ID> };
+    db.collection('notes').findOne(details, (err, item) => {
+      if (err) {
+        res.send({'error':'An error has occurred'});
+      } else {
+        res.send(item);
+      }
+    });
+  });
   app.post('/notes', (req, res) => {
     // Здесь будем создавать заметку.
     const note = { text: req.body.body, title: req.body.title };
