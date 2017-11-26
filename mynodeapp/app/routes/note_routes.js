2ba35b05271985a4734558d5775262e56aaac96e
ptflp <filippov.pa@s-vfu.ru>
2017-11-27 01:37:08 +0900

    sending id by Object, required by Mongodb

--- mynodeapp/app/routes/note_routes.js
+++ mynodeapp/app/routes/note_routes.js
@@ -1,6 +1,7 @@
 module.exports = function(app, db) {
   app.get('/notes/:id', (req, res) => {
-    const details = { '_id': <ТУТ БУДЕТ ID> };
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id) };
     db.collection('notes').findOne(details, (err, item) => {
       if (err) {
         res.send({'error':'An error has occurred'});
